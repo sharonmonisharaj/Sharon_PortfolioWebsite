ebee90050241173ca0d91e48e1042d32069859d3
sharonmonisharaj <sharonmonisharaj@gmail.com>
2016-12-04 20:09:47 -0500

    Got all the hover summaries to work

--- myapp.js
+++ myapp.js
@@ -1,4 +1,264 @@
-function myFunction() {
-	document.getElementById("elevator_details").style.display = "none";
-    document.getElementById("hidesign_details").style.display = "block";
+function prevNext() {
+
+	if(document.getElementById("elevator_details").style.display === "block"){
+		document.getElementById("initial_title").style.display = "none";
+		document.getElementById("elevator_details").style.display = "none";
+		document.getElementById("sketch_details").style.display = "none";
+    	document.getElementById("hidesign_details").style.display = "block";
+		document.getElementById("title_text").innerHTML = "Hidesign: Recycled Leather and Brass Product Design";
+		
+	} else if (document.getElementById("hidesign_details").style.display === "block") {
+		document.getElementById("initial_title").style.display = "none";
+		document.getElementById("hidesign_details").style.display = "none";
+		document.getElementById("elevator_details").style.display = "none";
+		document.getElementById("sketch_details").style.display = "block";
+		document.getElementById("title_text").innerHTML = "Hand-drawn Sketch Projects";
+	
+	} else {
+		document.getElementById("initial_title").style.display = "none";
+		document.getElementById("sketch_details").style.display = "none";
+		document.getElementById("hidesign_details").style.display = "none";
+		document.getElementById("elevator_details").style.display = "block";
+		document.getElementById("title_text").innerHTML = "Leg Elevator for Ankle Fracture Rehabilitation";
+	}
 }
+
+// ------------------------------------------------------------------
+// Product Design
+// ------------------------------------------------------------------
+
+function showSummary1() {
+	document.getElementById("hover_summary1").style.display = "block";
+}
+function hideSummary1() {
+	document.getElementById("hover_summary1").style.display = "none";
+}
+
+
+
+function showSummary2() {
+	document.getElementById("hover_summary2").style.display = "block";
+}
+function hideSummary2() {
+	document.getElementById("hover_summary2").style.display = "none";
+}
+
+function showSummary3() {
+	document.getElementById("hover_summary3").style.display = "block";
+}
+function hideSummary3() {
+	document.getElementById("hover_summary3").style.display = "none";
+}
+
+
+function showSummary4() {
+	document.getElementById("hover_summary4").style.display = "block";
+}
+function hideSummary4() {
+	document.getElementById("hover_summary4").style.display = "none";
+}
+
+
+function showSummary5() {
+	document.getElementById("hover_summary5").style.display = "block";
+}
+function hideSummary5() {
+	document.getElementById("hover_summary5").style.display = "none";
+}
+
+
+function showSummary6() {
+	document.getElementById("hover_summary6").style.display = "block";
+}
+function hideSummary6() {
+	document.getElementById("hover_summary6").style.display = "none";
+}
+
+
+function showSummary7() {
+	document.getElementById("hover_summary7").style.display = "block";
+}
+function hideSummary7() {
+	document.getElementById("hover_summary7").style.display = "none";
+}
+
+
+function showSummary8() {
+	document.getElementById("hover_summary8").style.display = "block";
+}
+function hideSummary8() {
+	document.getElementById("hover_summary8").style.display = "none";
+}
+
+// ------------------------------------------------------------------
+// Interaction Design
+// ------------------------------------------------------------------
+
+function showIntSummary1() {
+	document.getElementById("hover_int_summary1").style.display = "block";
+}
+function hideIntSummary1() {
+	document.getElementById("hover_int_summary1").style.display = "none";
+}
+
+
+function showIntSummary2() {
+	document.getElementById("hover_int_summary2").style.display = "block";
+}
+function hideIntSummary2() {
+	document.getElementById("hover_int_summary2").style.display = "none";
+}
+
+
+function showIntSummary3() {
+	document.getElementById("hover_int_summary3").style.display = "block";
+}
+function hideIntSummary3() {
+	document.getElementById("hover_int_summary3").style.display = "none";
+}
+
+
+function showIntSummary4() {
+	document.getElementById("hover_int_summary4").style.display = "block";
+}
+function hideIntSummary4() {
+	document.getElementById("hover_int_summary4").style.display = "none";
+}
+
+
+function showIntSummary5() {
+	document.getElementById("hover_int_summary5").style.display = "block";
+}
+function hideIntSummary5() {
+	document.getElementById("hover_int_summary5").style.display = "none";
+}
+
+
+function showIntSummary6() {
+	document.getElementById("hover_int_summary6").style.display = "block";
+}
+function hideIntSummary6() {
+	document.getElementById("hover_int_summary6").style.display = "none";
+}
+
+
+function showIntSummary7() {
+	document.getElementById("hover_int_summary7").style.display = "block";
+}
+function hideIntSummary7() {
+	document.getElementById("hover_int_summary7").style.display = "none";
+}
+
+
+function showIntSummary8() {
+	document.getElementById("hover_int_summary8").style.display = "block";
+}
+function hideIntSummary8() {
+	document.getElementById("hover_int_summary8").style.display = "none";
+}
+
+// ------------------------------------------------------------------
+// Sketching
+// ------------------------------------------------------------------
+
+function showSketchSummary1() {
+	document.getElementById("hover_sketch_summary1").style.display = "block";
+}
+function hideSketchSummary1() {
+	document.getElementById("hover_sketch_summary1").style.display = "none";
+}
+
+
+function showSketchSummary2() {
+	document.getElementById("hover_sketch_summary2").style.display = "block";
+}
+function hideSketchSummary2() {
+	document.getElementById("hover_sketch_summary2").style.display = "none";
+}
+
+
+function showSketchSummary3() {
+	document.getElementById("hover_sketch_summary3").style.display = "block";
+}
+function hideSketchSummary3() {
+	document.getElementById("hover_sketch_summary3").style.display = "none";
+}
+
+
+function showSketchSummary4() {
+	document.getElementById("hover_sketch_summary4").style.display = "block";
+}
+function hideSketchSummary4() {
+	document.getElementById("hover_sketch_summary4").style.display = "none";
+}
+
+
+function showSketchSummary5() {
+	document.getElementById("hover_sketch_summary5").style.display = "block";
+}
+function hideSketchSummary5() {
+	document.getElementById("hover_sketch_summary5").style.display = "none";
+}
+
+
+function showSketchSummary6() {
+	document.getElementById("hover_sketch_summary6").style.display = "block";
+}
+function hideSketchSummary6() {
+	document.getElementById("hover_sketch_summary6").style.display = "none";
+}
+
+
+function showSketchSummary7() {
+	document.getElementById("hover_sketch_summary7").style.display = "block";
+}
+function hideSketchSummary7() {
+	document.getElementById("hover_sketch_summary7").style.display = "none";
+}
+	
+// ------------------------------------------------------------------
+// Programming
+// ------------------------------------------------------------------
+
+function showProgSummary1() {
+	document.getElementById("hover_prog_summary1").style.display = "block";
+}
+function hideProgSummary1() {
+	document.getElementById("hover_prog_summary1").style.display = "none";
+}
+
+
+function showProgSummary2() {
+	document.getElementById("hover_prog_summary2").style.display = "block";
+}
+function hideProgSummary2() {
+	document.getElementById("hover_prog_summary2").style.display = "none";
+}
+
+
+function showProgSummary3() {
+	document.getElementById("hover_prog_summary3").style.display = "block";
+}
+function hideProgSummary3() {
+	document.getElementById("hover_prog_summary3").style.display = "none";
+}
+
+
+function showProgSummary4() {
+	document.getElementById("hover_prog_summary4").style.display = "block";
+}
+function hideProgSummary4() {
+	document.getElementById("hover_prog_summary4").style.display = "none";
+}
+
+
+function showProgSummary5() {
+	document.getElementById("hover_prog_summary5").style.display = "block";
+}
+function hideProgSummary5() {
+	document.getElementById("hover_prog_summary5").style.display = "none";
+}
+
+
+
+
